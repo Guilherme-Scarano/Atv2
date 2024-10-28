6f41aac969348715162393d5e11768e20997af56
Guilherme-Scarano <116522846+Guilherme-Scarano@users.noreply.github.com>
2024-10-28 20:12:48 -0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro texto: ");
+        string texto1 = Console.ReadLine();
+
+        Console.Write("Digite o segundo texto: ");
+        string texto2 = Console.ReadLine();
+
+        bool saoIguais = string.Compare(texto1.ToUpper(), texto2.ToUpper()) == 0;
+
+        if (saoIguais)
+        {
+            Console.WriteLine("Os textos são iguais (ignorando maiúsculas e minúsculas).");
+        }
+        else
+        {
+            Console.WriteLine("Os textos são diferentes.");
+        }
+
+        Console.WriteLine("Quantidade de caracteres do primeiro texto: " + texto1.Length);
+        Console.WriteLine("Quantidade de caracteres do segundo texto: " + texto2.Length);
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma palavra: ");
+        string palavra = Console.ReadLine();
+
+        Console.Write("Digite uma letra: ");
+        char letra = Console.ReadLine()[0];
+
+        palavra = palavra.ToUpper();
+        letra = Char.ToUpper(letra);
+
+        int contador = 0;
+        foreach (char c in palavra)
+        {
+            if (c == letra)
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("A letra '" + letra + "' aparece " + contador + " vez(es) na palavra.");
+    }
+}
